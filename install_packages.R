d1cdb676e56c6261b781662a0638f2fede2d554f
Eric R <ryninho@users.noreply.github.com>
2015-08-04 12:55:01 -0700

    install packages

---
+++ install_packages.R
@@ -0,0 +1,50 @@
+options(repos=structure(c(CRAN="http://cran.cnr.Berkeley.edu")))
+#needed packages
+for (pckg in c("forecast"
+               , "sqldf"
+               , "httr"
+               , "RPostgreSQL"
+               , "parallel"
+               , "reshape"
+               , "zoo"
+               , "plyr"
+               , "MASS"
+               , "lme4"
+               , "mvoutlier"
+               , "lubridate"
+               , "jsonlite"
+               , "Hmisc"
+               , "dplyr"
+               , "rjson"
+               , "pander"
+               , "xtable"
+               , "rredis"
+               , "caroline"
+               , "magrittr"
+)
+     ){
+  if (!pckg %in% rownames(installed.packages())) {install.packages(pckg)}
+}
+
+#other packages
+for (pckg in c("data.table"
+               , "mgcv"
+               , "reshape2"
+               , "lattice"
+               , "latticeExtra"
+               , "gridExtra"
+               , "fasttime"
+               , "randomForest"
+               , "sendmailR"
+               , "ggplot2"
+               , "scales"
+               , "rPython"
+               , "grid"
+               , "Formula"
+               , "splines"
+               , "survival"
+               , "glmnet"
+)
+     ){
+  if (!pckg %in% rownames(installed.packages())) {install.packages(pckg)}
+}
