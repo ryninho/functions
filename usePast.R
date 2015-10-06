47798a467e8270d3b033da44924ea087f09fd672
Eric R <ryninho@users.noreply.github.com>
2015-10-05 20:49:25 -0700

    Update usePast.R

--- usePast.R
+++ usePast.R
@@ -1,6 +1,8 @@
 usePast <- function(query, past_date) {
-## this is useful for running a SQL query as if it were a previous date ##
-## ... works well if you have a script that takes a date as a           ##
-## command line input and runs SQL queries as part of the process       ##
+## this is useful for running a SQL query as if it were a previous date      ##
+## ... works well if you have a script that takes a date as a                ##
+## command line input and runs SQL queries as part of the process            ##
+## e.g. historical <- ifelse(length(cmd_args) > 0, TRUE, FALSE)              ##
+## dbGetQuery2(con, ifelse(historical, usePast(sql_qry, cmd_date), sql_qry)) ##
   gsub("current_date", paste0("DATE '", past_date, "'"), query)
 }
