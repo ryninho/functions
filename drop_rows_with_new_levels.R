d0d683a8103fa13b37f04467c148d9d8fe2b2617
Eric R <ryninho@users.noreply.github.com>
2015-11-15 18:48:48 -0800

    Update drop_rows_with_new_levels.R

--- drop_rows_with_new_levels.R
+++ drop_rows_with_new_levels.R
@@ -3,7 +3,6 @@ drop_rows_with_new_levels <- function(lm_object, newdata) {
   factor_cols <- names(lm_object$xlevels)
   old_data <- 
     for (x in factor_cols) {
-      print(sum(df[,x] %in% lm1$model[,x]))
       df <- df[df[,x] %in% lm1$model[,x],]
     }
   return(df)
