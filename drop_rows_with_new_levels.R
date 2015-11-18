15429eb61e38d70a15d8a2818c07449c967877ba
Eric R <ryninho@users.noreply.github.com>
2015-11-18 14:49:28 -0800

    Update drop_rows_with_new_levels.R

--- drop_rows_with_new_levels.R
+++ drop_rows_with_new_levels.R
@@ -1,9 +1,20 @@
-drop_rows_with_new_levels <- function(lm_object, newdata) {
+#' Drop rows in dataframe with factor levels unaccounted for in a linear model.
+#' 
+#' @param newdata A data frame of new values to score with the linear model.
+#' @param lm_object A linear model.
+#' @return The data frame of new values stripped of rows with brand new factors.
+#' @examples
+#' drop_rows_with_new_levels(tail(OrchardSprays), 
+#'                           lm(decrease ~ treatment, 
+#'                              data = head(OrchardSprays)
+#'                           )
+#' )
+drop_rows_with_new_levels <- function(newdata, lm_object) {
   df <- newdata
   factor_cols <- names(lm_object$xlevels)
-  old_data <- 
+  old_data <-
     for (x in factor_cols) {
-      df <- df[df[,x] %in% lm1$model[,x],]
+      df <- df[df[,x] %in% lm_object$model[,x],]
     }
   return(df)
 }
