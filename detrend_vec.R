78c13a7e833bef62eafb52bcfd6207629ea9cf4b
Eric R <ryninho@users.noreply.github.com>
2017-10-01 21:07:53 -0700

    Update detrend_vec.R

--- detrend_vec.R
+++ detrend_vec.R
@@ -1,8 +1,19 @@
-# Detrend a vector (converts to/from time series)
-
-detrend_vec <- function(vec, frequency = 7) {
-  series <- ts(vec, frequency = frequency)
+#' Detrend a vector (esp. of demand)
+#' 
+#' @param vec A numeric vector
+#' @param freq The seasonal period of the vector (used to construct time series)
+#' @return A numeric vector with the seasonality preserved but trend flattened
+detrend_vec <- function(vec, freq = 7) {
+  series <- ts(vec, frequency = freq)
   trend = stl(series, s.window = "periodic")$time.series[,2]
   detrend_ts = series - (trend - trend[1])
   return(as.numeric(detrend_ts))
 }
+
+detrend_demand <- function(df) {
+  df$demand_flat <- detrend_vec(df$demand)
+  return(df)
+}
+
+# Example useage:
+# hw_fcst %>% arrange(zone_id, date) %>% group_by(zone_id) %>% do(detrend_demand(.))
